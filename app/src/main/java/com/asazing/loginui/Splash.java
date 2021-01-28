81c1099b2f28d046917a11f7b53d21426c2d8964
ASAZING <malcolmedina206@gmail.com>
2021-01-28 17:42:57 -0500

    add splash an animations

--- app/src/main/java/com/asazing/loginui/Splash.java
+++ app/src/main/java/com/asazing/loginui/Splash.java
@@ -5,16 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class Splash extends Activity {
+    ImageView logo;
+    TextView loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.logoSplash);
+        loading = findViewById(R.id.loading);
+        Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+        Animation blink = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+        logo.startAnimation(rotate);
+        loading.startAnimation(blink);
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
