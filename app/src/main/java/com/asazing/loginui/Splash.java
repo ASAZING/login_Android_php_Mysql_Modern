b6282af8b8ce0a4b3e6849ea56b61b0e12b241c8
ASAZING <malcolmedina206@gmail.com>
2021-01-28 17:07:29 -0500

    add splash

---
+++ app/src/main/java/com/asazing/loginui/Splash.java
@@ -0,0 +1,30 @@
+package com.asazing.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 6000);
+    }
+}
